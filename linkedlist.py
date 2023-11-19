af1d04a4a27ad4b5e2958ea6d7af4f647cb0fbbc
Your Name <juanes1793@gmail.com>
2023-11-19 00:34:47 -0500

    pop method append

--- linkedlist.py
+++ linkedlist.py
@@ -27,9 +27,51 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+
+my_linked_list = LinkedList(1)
+#my_linked_list.make_empty()
+
+my_linked_list.append(1)
+my_linked_list.append(2)
+
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length, '\n')
+
+print('Linked List:')
+my_linked_list.print_list()
 
 
 
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 1
+    Tail: 2
+    Length: 2 
+
+    Linked List:
+    1
+    2
+    
+"""
 
 
 
