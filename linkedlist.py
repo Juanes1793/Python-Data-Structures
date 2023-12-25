cd97a38e5222948f2893332d7264624a1a63ceff
Your Name <juanes1793@gmail.com>
2023-12-25 12:48:59 -0500

    minor change

--- linkedlist.py
+++ linkedlist.py
@@ -43,6 +43,31 @@ class LinkedList:
             self.head = None
             self.tail = None
         return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        
+        
+
+
+
 
 
 my_linked_list = LinkedList(1)
