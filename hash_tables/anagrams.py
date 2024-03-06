e3cf36d01ffce70c8f398c72a9c2c92b9e43244c
Juan Esteban López Cardona <jelopez@igerencia.com>
2024-03-05 22:14:57 -0500

    hash table exercises

---
+++ hash_tables/anagrams.py
@@ -0,0 +1,35 @@
+def group_anagrams(words):
+    anagrams = {}
+    for word in words:
+        key = ''.join(sorted(word))
+        if key in anagrams:
+            anagrams[key].append(word)
+        else:
+            anagrams[key] = [word]
+    return list(anagrams.values())
+
+
+print("1st set:")
+print( group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) )
+
+print("\n2nd set:")
+print( group_anagrams(["abc", "cba", "bac", "foo", "bar"]) )
+
+print("\n3rd set:")
+print( group_anagrams(["listen", "silent", "triangle", "integral", "garden", "ranged"]) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1st set:
+    [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+
+    2nd set:
+    [['abc', 'cba', 'bac'], ['foo'], ['bar']]
+
+    3rd set:
+    [['listen', 'silent'], ['triangle', 'integral'], ['garden', 'ranged']]
+
+"""
